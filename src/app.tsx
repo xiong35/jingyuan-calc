21f6cf4a032bdf6c2edc7cfcc16a1942275589a2
xiong35 <xiong35@qq.com>
2023-05-23 12:11:05 +0800

    feat(fe): 添加组件库

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import "./app.css";
 
+import Button from "preact-material-components/Button";
+import "preact-material-components/Button/style.css";
+import "preact-material-components/Theme/style.css";
+// https://material.preactjs.com/component/button/
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>景元伤害计算器</h1>
+
+      <form>
+        <Button ripple raised>
+          Flat button with ripple
+        </Button>
+        <Button ripple raised className="mdc-theme--primary-bg">
+          Primary background button
+        </Button>
+        <Button href="/">This button will be rendered as an anchor</Button>
+      </form>
+    </main>
+  );
 }
